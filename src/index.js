cf8f268676250e0f11f391bb38269f507cece000
Gokulraj12 <gokul799@gmail.com>
2021-01-29 10:50:50 +0530

    inlinestyling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 //function Component App definition
 const App = () => {
   return (
-    <div>
+    <div style={{ backgroundColor: 'blueviolet', color: 'whitesmoke' }}>  {/*Inline Styling using JSX*/}
       <h1>Testing develop branch</h1>
     </div>
   );
