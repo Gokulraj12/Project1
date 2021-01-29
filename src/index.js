643e0241dfe54a6c7a0e57addb27ad9c2584540c
Gokulraj12 <gokul799@gmail.com>
2021-01-29 08:22:06 +0530

    Test the develop commits

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 //function Component App definition
 const App = () => {
   return (
-    <div>Hi There!</div>
+    <div>
+      <h1>Testing develop branch</h1>
+    </div>
   );
 }
 ReactDOM.render(
